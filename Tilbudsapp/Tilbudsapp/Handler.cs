34ec622e4b5c377b5dc99617bcb2ffd802259c53
Niko <Nikol@DESKTOP-NRSH0JP>
2016-09-29 12:54:13 +0200

    Jeg har lavet 2 nye klasser

--- Tilbudsapp/Tilbudsapp/Handler.cs
+++ Tilbudsapp/Tilbudsapp/Handler.cs
@@ -22,7 +22,7 @@ namespace Tilbudsapp
                 var Vare = facade.GetListAsync(new Vare()).Result;
 
 
-                var vare2 = facade.GetListAsync(new Vare()).Result;
+               
                 Console.WriteLine("Skriv hele eller dele af varenavnet");
                 string søgeord = Console.ReadLine();
 
@@ -61,6 +61,40 @@ namespace Tilbudsapp
             {
                 Console.WriteLine(e + e.Message);
             }
+
+        }
+
+        public static void søgekampagne()
+        {
+            var tildbud = facade.GetListAsync(new Tilbud()).Result;
+            var kæde = facade.GetListAsync(new Kaede()).Result;
+            var Vare = facade.GetListAsync(new Vare()).Result;
+            var Tilbudkampagne = facade.GetListAsync(new TilbudKampagne()).Result;
+            var kampagne = facade.GetListAsync(new Kampagne()).Result;
+
+
+            Console.WriteLine("Skriv hele eller dele af varenavnet");
+            string søgeord = Console.ReadLine();
+
+
+            if (søgeord == null)
+            {
+                søgeord = "";
+            }
+
+            var søgtevare = from k in kampagne where (k.Navn.ToLower().Contains(søgeord)) select k;
+
+            var tilbudsliste = from k in søgtevare
+                               join t in kæde on k.Fk_kaede_ID equals t.Kaede_ID
+                               select new { k, t.Kaede_ID,  };
+
+
+
+
+
+
+
+
 
         }
     }
---
+++ Tilbudsapp/Tilbudsapp/TilbudKampagne.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tilbudsapp
+{
+    class TilbudKampagne : IWebUri
+    {
+
+        public int Fk_kampagne_ID { get; set; }
+
+        public int Fk_tilbud { get; set; }
+
+
+        public TilbudKampagne(int fkKampagneId, int fkTilbud)
+        {
+            Fk_kampagne_ID = fkKampagneId;
+            Fk_tilbud = fkTilbud;
+        }
+
+    
+        public TilbudKampagne()
+        {
+            ResourceUri = "TilbudKampagnes";
+            VerboseName = "TilbudKampagne";
+
+        }
+
+        public string ResourceUri { get; }
+        public string VerboseName { get; }
+    }
+}
+
+
+
